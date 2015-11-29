a0fd496d6dfa7a5280139cf24ea34f6ddac234dc
Nelson Melo <nmelo.cu@gmail.com>
2015-11-29 13:32:26 -0500

    fixing package name

--- package.js
+++ package.js
@@ -2,11 +2,11 @@ Package.describe({
   name: 'nmelo:google-maps-cordova',
   summary: 'Native Google Maps for Cordova',
   git: 'https://github.com/nmelo/meteor-google-maps-cordova.git',
-  version: '0.1.6'
+  version: '0.1.7'
 });
 
 Cordova.depends({
-  'cordova-plugin-googlemaps': '1.3.4'
+  'plugin.google.maps': 'https://github.com/mapsplugin/cordova-plugin-googlemaps/commit/d59b5521f3297a406fe68d487eef13516bad033a'
 });
 
 Package.onUse(function(api) {
